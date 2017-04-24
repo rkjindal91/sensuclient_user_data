667269accf797701f351ce49e1e8d6232797e778
rkjindal91 <rkjindal91@gmail.com>
2017-04-24 15:57:54 +0530

    24Ap04

--- userdatashort.sh
+++ userdatashort.sh
@@ -2,6 +2,6 @@
 set -e -x
 export DEBIAN_FRONTEND=noninteractive
 MYNAME=$(hostname)
-echo -e '''{\n"rabbitmq":{\n"host":"52.220.142.250",\n"port":5672,\n"vhost":"/sensu",\n"user":"sensu",\n"password":"secret"\n}\n}''' | tee /etc/sensu/config.d/rabbitmq.json
+echo '''{\n"rabbitmq":{\n"host":"<<PUBLIC_IP_SENSUMASTER>>",\n"port":5672,\n"vhost":"/sensu",\n"user":"<<USERNAME>>",\n"password":"<<PASSWORD>>"\n}\n}''' | tee /etc/sensu/conf.d/rabbitmq.json
 MYIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-echo -e '''{\n"client":{\n"name":"'''$MYNAME'''",\n"address":"'''$MYIP'''",\n"subscriptions":["ALL","HTTPD","two_core_system","SSHD","NODEJS"],\n"safe_mode":false\n}\n}''' | tee /etc/sensu/config.d/client.json
+echo '''{\n"client":{\n"name":"'''$MYNAME'''",\n"address":"'''$MYIP'''",\n"subscriptions":["ALL"],\n"safe_mode":false\n}\n}''' | tee /etc/sensu/conf.d/client.json
