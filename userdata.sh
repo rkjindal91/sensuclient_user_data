f7e6a983b07f3d5fc873997f757e0d4bf38a7bab
rkjindal91 <rkjindal91@gmail.com>
2017-04-24 15:13:19 +0530

    24Ap03

--- userdata.sh
+++ userdata.sh
@@ -6,3 +6,6 @@ echo "deb http://repos.sensuapp.org/apt xenial main" | sudo tee /etc/apt/sources
 apt-get update && apt-get upgrade -y
 apt-get install sensu -y
 echo -e '''{\n"rabbitmq":{\n"host":"54.173.204.175",\n"port":5672,\n"vhost":"/sensu",\n"user":"sensu",\n"password":"secret"\n}\n}''' | sudo tee /etc/sensu/config.d/rabbitmq.json
+MYIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+echo -e '''{\n"client":{\n"name":"TestServer1",\n"address":"'''$MYIP'''",\n"subscriptions":["ALL","HTTPD","two_core_system","SSHD","NODEJS"],\n"safe_mode":false\n}\n}''' | sudo tee /etc/sensu/config.d/client.json
+
