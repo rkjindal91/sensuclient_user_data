9df3d66241222ec8f6d256b81735f7be7b663ee4
rkjindal91 <rkjindal91@gmail.com>
2017-04-29 17:38:49 +0530

    Added installation of bind-utils

--- userdata-01.sh
+++ userdata-01.sh
@@ -1,19 +1,22 @@
 #################################
 #				#
 # -Following script will change	#
-# name & address of client.json	#
+# name & address of client.json	#	
 # file located at 		#
 # /etc/sensu/conf.d/		#
 # -For error resolution	mail at	#
-# mailrkjindal@gmail.com	#
+# rajkumar.jindal@minjar.com	#
 #				#
 #################################
 
 #!/bin/bash
 set -e -x
 export DEBIAN_FRONTEND=noninteractive
+yum update -y
+yum install bind-utils -y
 MYNAME=$(hostname)
 sed -i "s/^\"name\"\:.*$/\"name\"\:\"$MYNAME\"\,/g" /etc/sensu/conf.d/client.json
 
 MYIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 sed -i "s/^\"address\"\:.*$/\"address\"\:\"$MYIP\"\,/g" /etc/sensu/conf.d/client.json
+service sensu-client restart
